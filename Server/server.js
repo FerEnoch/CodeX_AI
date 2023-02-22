117d8f9eb209d0e130fcb12e1d26a06ef6b10116
Fer Enoch <ferenochsoames@gmail.com>
2023-02-22 20:07:20 -0300

    Port server fixed

--- Server/server.js
+++ Server/server.js
@@ -49,5 +49,4 @@ app.post('/', async (req, res) => {
     }
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log('Server is running on port http://localhost:' + port));
+app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
